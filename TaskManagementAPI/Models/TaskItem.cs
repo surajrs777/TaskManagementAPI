913e76ddce1a9b98efa55702fbf32c7d26e0c591
Suraj <Suraj@LAPTOP-S9DCT178>
2026-04-12 14:30:28 +0530

    Added models for user and task item.

---
+++ TaskManagementAPI/Models/TaskItem.cs
@@ -0,0 +1,20 @@
+﻿namespace TaskManagementAPI.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }  = false;
+        public int Priority { get; set; } = 1; // 1 = Low, 2 = Medium, 3 = High
+        public DateTime? DueDate { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Foreign key
+
+        public DateTime UserId { get; set; }
+
+        // Navigation property
+        public User User { get; set; } = null!;
+    }
+}
---
+++ TaskManagementAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManagementAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation property
+
+        public ICollection<TaskItem> users { get; set; } = new List<TaskItem>();
+    }
+}
